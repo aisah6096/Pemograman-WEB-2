48784087719e7af74871617d1890ade12ca00af8
aisah6096 <80568632+aisah6096@users.noreply.github.com>
2021-05-26 21:48:16 +0700

    Add files via upload

---
+++ Pertemuan 12/Pert12_Lat1.php
@@ -0,0 +1,16 @@
+<?php
+
+$con = mysqli_connect("localhost","root","");  
+if (!$con) 
+ 	{ 
+ 	die('Could not connect: ' . mysqli_error()); 
+ 	} 
+mysqli_select_db($con, "lat_dbase");
+
+mysqli_query($con, "UPDATE tbl_mhs SET Age = '36' 
+WHERE FirstName = 'Karina' AND LastName = 'Suwandi'"); 
+mysqli_close($con);
+
+echo "<br><br>Nama : Siti Aisah";
+echo "<br>NIM &ensp;: 181011401068";
+?> 
